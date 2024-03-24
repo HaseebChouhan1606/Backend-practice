33d429afc8fff56e976d9aa397894686ebe66d15
HaseebChouhan1606 <haseebchouhan61@gmail.com>
2024-03-24 06:04:01 +0500

    cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,8 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"
+          
+cloudinary.config({ 
+  cloud_name: 'dlsbanrp5', 
+  api_key: '592739116853522', 
+  api_secret: 'sI5Srx445KMcuIlYeZ2lMhnfcJU' 
+});
